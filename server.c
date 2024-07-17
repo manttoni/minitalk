edf1106e6408970e8595054ad4f9685a24df0bd5
Anttoni Maula <manttoni92@gmail.com>
2024-07-17 12:34:24 +0300

    fix

--- server.c
+++ server.c
@@ -28,17 +28,19 @@ static void handler(int sig, siginfo_t *si, void *unused)
 	sigset_t set;
 	sigset_t oldset;
 
+	if (next.sender_pid == 0)
+		next.sender_pid = si->si_pid;
+	if (next.sender_pid != si->si_pid)
+		return ;
 	sigemptyset(&set);
 	sigaddset(&set, SIGUSR1);
 	sigaddset(&set, SIGUSR2);
 	sigprocmask(SIG_BLOCK, &set, &oldset);
-	if (next.sender_pid != si->si_pid && next.sender_pid != 0)
-		return ;
 	if (sig == SIGUSR2)
 		onebit();
 	else if (sig == SIGUSR1)
 		zerobit();
-	kill(si->si_pid, SIGUSR1);
+	kill(next.sender_pid, SIGUSR1);
 	if (next.bits == 8)
 	{
 		write(1, &next.byte, 1);
